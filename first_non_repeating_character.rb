346ba5abea5158dab81d5005751cf25401d02441
Tyler Rhodes <rhodetyl000@gmail.com>
2021-04-02 21:24:09 -0700

    Add solution for min waiting time

---
+++ first_non_repeating_character.rb
@@ -0,0 +1,13 @@
+def first_non_repeating_character(string)
+  split_str = string.split('')
+  
+  storage = split_str.inject({}) do |acc, char| 
+    acc[char] ? acc[char] += 1 : acc[char] = 1
+    acc
+  end
+
+  split_str.each_with_index { |char, i| return i if storage[char] == 1 }
+  return -1
+end
+
+puts first_non_repeating_character('abcdcaf')
