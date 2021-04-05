1f36084f896cc62619f433c6497f83f5f0d0b6cf
Tyler Rhodes <rhodetyl000@gmail.com>
2021-04-04 21:11:57 -0700

    Add solution for sorted squared array

---
+++ sorted_squared_array.rb
@@ -0,0 +1,19 @@
+def sorted_squared_array(array)
+  array.map { |v| v * v }.sort!
+end
+
+def sorted_squared_arr(array)
+  result = Array.new(array.length) { |i| 0 }
+  left = 0
+  right = array.length - 1
+  for i in (array.length - 1).downto(0) do
+    if (array[left]).abs > (array[right]).abs
+      result[i] = array[left] * array[left]
+      left += 1
+    else
+      result[i] = array[right] * array[right]
+      right -= 1
+    end
+  end
+  result
+end
---
+++ spec/sorted_squared_array_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../sorted_squared_array'
+
+describe 'sorted_squared_array' do
+  before do
+    @example1 = [1, 2, 3, 5, 6, 8, 9]
+    @example2 = [-5, -4, -3, -2, -1]
+  end
+
+  it "should return the values sorted and squared" do
+    expect(sorted_squared_array(@example1)).to eq([1, 4, 9, 25, 36, 64, 81])
+    expect(sorted_squared_array(@example2)).to eq([1, 4, 9, 16, 25])
+
+    expect(sorted_squared_arr(@example1)).to eq([1, 4, 9, 25, 36, 64, 81])
+    expect(sorted_squared_arr(@example2)).to eq([1, 4, 9, 16, 25])
+  end
+end
