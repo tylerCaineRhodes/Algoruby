a6dad4b2ab941d1c30e627e4c06f8b9cb005ab7a
Tyler Rhodes <rhodetyl000@gmail.com>
2021-04-06 19:24:17 -0700

    Add solution to tandem bicycle

---
+++ tandem_bicycle.rb
@@ -0,0 +1,5 @@
+def tandem_bicycle(red_shirt_speeds, blue_shirt_speeds, fastest)
+  red_shirt_speeds.sort!
+  fastest ? blue_shirt_speeds.sort! { |a,b| b - a } : blue_shirt_speeds.sort! { |a,b| a - b } 
+  red_shirt_speeds.map.with_index { |val, i| [red_shirt_speeds[i], blue_shirt_speeds[i]].max }.inject(:+)
+end
