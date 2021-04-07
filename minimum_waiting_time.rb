094da1f5343765fcee0723d83faae4733e20637a
Tyler Rhodes <rhodetyl000@gmail.com>
2021-04-06 19:27:21 -0700

    Use blocks in conventional ruby way

--- minimum_waiting_time.rb
+++ minimum_waiting_time.rb
@@ -6,14 +6,14 @@ def minimum_waiting_time(queries)
   for i in (1...queries.length)
     times[i] = times[i - 1] + queries[i - 1]
   end
-   times.inject(0) { |acc, val| acc + val }
+   times.inject(:+)
 end
 
 #solution2
 def min_waiting_time(queries)
   queries.sort!
   totalTime = 0;
-  queries.each_with_index do |item, i|
+  queries.length.times do |i|
     queries_left = queries.length - (i + 1)
     totalTime += queries[i] * queries_left
   end
--- sorted_squared_array.rb
+++ sorted_squared_array.rb
@@ -6,7 +6,7 @@ def sorted_squared_arr(array)
   result = Array.new(array.length) { |i| 0 }
   left = 0
   right = array.length - 1
-  for i in (array.length - 1).downto(0) do
+  (array.length - 1).downto(0) do |i|
     if (array[left]).abs > (array[right]).abs
       result[i] = array[left] * array[left]
       left += 1
